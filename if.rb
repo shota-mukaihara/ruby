c721d5299cf259983d86b10e6f831e5a428342e8
shota-mukaihara <mukaihara.shota.infratop@gmail.com>
2020-03-08 22:57:53 +0900

    finish2-7

--- if.rb
+++ if.rb
@@ -1,7 +1,35 @@
 apple = 110
 
-apple >=100
+if apple >=100
 puts "リンゴの値段は100円以上です。"
+end
 
-apple <=100
+if apple <=100
 puts "リンゴの値段は100円以下です。"
+end
+
+
+tall = 180
+
+if tall >=170 && tall >=190
+  puts "身長は170以上190以下です。"
+end
+
+
+apple = "Aomori"
+
+if apple == "Nagano"
+    puts "りんごの生産地は長野です"
+else
+    puts "りんごの生産地は青森です"
+end
+
+
+apple = "Yamagata"
+
+if apple == "Nagano"
+    puts "りんごの生産地は長野です"
+elsif apple == "Yamagata"
+    puts "りんごの生産地は山形です"
+else puts "りんごの生産地は青森です"
+end
